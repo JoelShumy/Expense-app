31fdf8bb16bc2cb4885d44f66f4ea0f7b46dd9d9
joel shum <joel.shumkwoong@gmail.com>
2021-06-04 16:39:08 +0200

    supperssion de ligne avec le bouton delete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-
+const tableBody = document.getElementById('tableResult');
+const table = document.querySelector('table')
 
 function addExpense(event){
 /*
@@ -10,21 +10,22 @@ function addExpense(event){
     ligne.appendChild(cellule);
     resultTable.appendChild(ligne);*/
 
-    const table = document.getElementById('tableResult');
+
 
     let nameExpense = document.getElementById('nameAmount');
     let dateExpense = document.getElementById('dateAmount');
     let amountExpense = document.getElementById('amount_to_ad');
 
-    const newRow = table.insertRow(table.rows.length);
+    const newRow = tableBody.insertRow(tableBody.rows.length);
     let cel1 = newRow.insertCell(0)
     let cel2 = newRow.insertCell(1)
     let cel3 = newRow.insertCell(2)
+    let cel4 = newRow.insertCell(3)
 
     cel1.innerHTML = nameExpense.value;
     cel2.innerHTML = dateExpense.value;
     cel3.innerHTML = amountExpense.value;
-
+    cel4.innerHTML = "<button class='deleteBtn'>Delete</button>"
     
     nameExpense.value ='';
     dateExpense.value ='';
@@ -33,8 +34,19 @@ function addExpense(event){
     event.preventDefault();
 }
 
+function deleteRow(e){
 
+    if(e.target.classList.contains('deleteBtn')) {
+        const btn = e.target;
+        btn.closest("tr").remove();
+    } else {
+        alert('ceci n"est pas un bouton supprimé')
+    }
+}
 
+// https://www.w3schools.com/jsref/met_table_deleterow.asp
+
+table.addEventListener('click', deleteRow)
 const add = document.getElementById('submit');
 add.addEventListener('click', addExpense);
 
